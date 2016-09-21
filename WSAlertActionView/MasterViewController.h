24e651e306976dd1894c73f8c2c9c2a408575025
wangkecheng <6780894753@qq.com>
2016-09-21 17:35:20 +0800

    Initial Commit

---
+++ WSAlertActionView/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  WSAlertActionView
+//
+//  Created by warron on 16/9/21.
+//  Copyright © 2016年 warron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
